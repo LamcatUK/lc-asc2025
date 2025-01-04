300dfed6ee25eb512a8a9fc9b3245afb65b8a65c
ChillibyteUK <136230172+LamcatUK@users.noreply.github.com>
2025-01-04 17:40:15 +0000

    Update lc_hero.php

--- page-templates/blocks/lc_hero.php
+++ page-templates/blocks/lc_hero.php
@@ -3,7 +3,7 @@
         <?= wp_get_attachment_image(get_field('background'), 'full', false, ['class' => 'hero__image', 'data-aos' => 'fade-down']) ?>
         <div class="overlay"></div>
         <div class="hero__title" data-aos="fade-up">
-            <h1 class="mb-0"><?= get_field('title') ?></h1>
+            <h1 class="mb-0 text-center"><?= get_field('title') ?></h1>
         </div>
     </div>
 </section>
